f82db43be77310dd6c4fd17f1677fca642767985
Amaury Carrade <amaury@carrade.eu>
2017-02-05 02:38:51 +0100

    Search engine nows searches per word - and added redirect from /<search term> to the search or tea page.
    * NEW: changed the search request to search per word (bu all words has to be there, so e.g. one can search for « opera red » and find « red opera ».
    * NEW: https://my-tea.pary/anything will redirect to the search for what typed, or the tea page if there is only one result (or a 404 with no result).

--- myteaparty/views.py
+++ myteaparty/views.py
@@ -1,6 +1,6 @@
 import math
 
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for, abort
 from peewee import SQL, fn
 from playhouse.flask_utils import get_object_or_404
 
@@ -34,12 +34,24 @@ def search_for_tea(search_query, paginate_by=0, page=1):
     with the results.
 
     If paginate_by and page are given (and positive), paginates the
-    results and returns a tuple with the peewee query and the pages
-    count. If search_query evaluates to False, returns [] instead of
-    a peewee query.
+    results and returns a tuple with the peewee query, the total number
+    of results and the pages count. If search_query evaluates to False,
+    returns [] instead of a peewee query.
     """
     if not search_query:
-        return [] if paginate_by <= 0 else [], 0
+        return [] if paginate_by <= 0 else [], 0, 0
+
+    search_terms = search_query.split()
+
+    relevance = SQL('0')
+    where_clause = SQL('1')
+    for word in search_terms:
+        relevance += (fn.IF(Tea.name.contains(word), app.config['SEARCH_WEIGHTS']['name'], 0) +
+                fn.IF(Tea.description.contains(word), app.config['SEARCH_WEIGHTS']['desc'], 0) +
+                fn.IF(Tea.long_description.contains(word), app.config['SEARCH_WEIGHTS']['ldesc'], 0))
+        where_clause &= (Tea.name.contains(word) |
+                Tea.description.contains(word) |
+                Tea.long_description.contains(word))
 
     teas = (Tea
             .select(
@@ -54,26 +66,22 @@ def search_for_tea(search_query, paginate_by=0, page=1):
                 Tea.tips_temperature,
                 TeaVendor.name.alias('vendor_name'),
                 TeaVendor.slug.alias('vendor_slug'),
-                (
-                    fn.IF(Tea.name.contains(search_query), app.config['SEARCH_WEIGHTS']['name'], 0)
-                    + fn.IF(Tea.description.contains(search_query), app.config['SEARCH_WEIGHTS']['desc'], 0)
-                    + fn.IF(Tea.long_description.contains(search_query), app.config['SEARCH_WEIGHTS']['ldesc'], 0)
-                ).alias('relevance'))
+                relevance.alias('relevance'))
             .join(TeaVendor)
+            .where(where_clause)
             .having(SQL('relevance') != 0)
             .order_by(SQL('relevance DESC')))
 
     if paginate_by > 0:
-        count = (Tea.select()
-                    .where(Tea.name.contains(search_query) |
-                           Tea.description.contains(search_query) |
-                           Tea.long_description.contains(search_query))
-                    .count())
-
+        count = Tea.select().where(where_clause).count()
         pages_count = int(math.ceil(float(count) / paginate_by))
+
+        if page != 1 and page > pages_count:
+            abort(404)
+
         teas = teas.paginate(page, paginate_by)
 
-    return teas if paginate_by <= 0 else (teas, pages_count)
+    return teas if paginate_by <= 0 else (teas, count, pages_count)
 
 
 @app.route('/search')
@@ -81,12 +89,28 @@ def search():
     search_query = request.args.get('q')
     page = request.args.get('page')
 
-    page = int(page) if page and page.isdigit() else 1
-    page = page if page >= 1 else 1
+    page = min(1, int(page) if page and page.isdigit() else 1)
 
-    teas, pages_count = search_for_tea(search_query, paginate_by=app.config['ITEMS_PER_PAGE'], page=page)
+    teas, count, pages_count = search_for_tea(search_query, paginate_by=app.config['ITEMS_PER_PAGE'], page=page)
+
+    if count == 1:
+        tea = teas.dicts()[0]
+        return redirect(url_for('tea', tea_slug=tea['slug'], tea_vendor=tea['vendor_slug']), 302)
 
     return render_template('search.html', search_query=search_query, teas=teas, pagination={
         'page': page,
         'pages': pages_count
     })
+
+
+@app.route('/<search_fallback>')
+def search_generic_fallback(search_fallback):
+    teas, count, _ = search_for_tea(search_fallback, paginate_by=2)
+
+    if count > 1:
+        return redirect(url_for('search', q=search_fallback), 302)
+    elif count == 1:
+        tea = teas.dicts()[0]
+        return redirect(url_for('tea', tea_slug=tea['slug'], tea_vendor=tea['vendor_slug']), 302)
+    else:
+        abort(404)
